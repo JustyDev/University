491691881aadcfa784fceb904076e1039047b904
Dmitry Sidorov <61616100+JustyDev@users.noreply.github.com>
2024-04-23 12:15:08 +0300

    задания теорвер

--- Semester-2/Теория вероятностей и матстат (16)/2 лаба/Решение/17.py
+++ Semester-2/Теория вероятностей и матстат (16)/2 лаба/Решение/17.py
@@ -1,25 +1,24 @@
 import time
-from random import uniform
+from random import random
+from math import sqrt
 
 # измерение времени работы
 start_time = time.time()
 m = 0  # успешные результаты
 
 N = 10_000_000
-T = 100
-t = 30
+l = 1  # длина катетов AB и AC, условие
 
 for _ in range(N):
-    # случайная генерация x и y
-    x, y = uniform(1, T), uniform(1, T)
-
-    # входят ли x и y в область шестиугольника
-    if (x + t > y > x) or (x - t < y < x):
+    # случайная генерация положения M
+    M = random()
+    # если сумма длин сторон меньше 3-х
+    if (M + l + sqrt(M ** 2 + l ** 2)) < 3:
         m += 1
 
 # расчёт времени выполнения
 execution_time = time.time() - start_time  # вычисляем время выполнения
 
 print("Имитация:", m / N)
-print("Ручной расчёт:", t * (2 * T - t) / T ** 2)
+print("Ручной расчёт:", 3/4)
 print(f"Время выполнения: {execution_time} секунд")
---
+++ Semester-2/Теория вероятностей и матстат (16)/3 лаба/16-1.py
@@ -0,0 +1,3 @@
+from numpy import random
+
+random.binomial(n=10, p=.25, size=10)
