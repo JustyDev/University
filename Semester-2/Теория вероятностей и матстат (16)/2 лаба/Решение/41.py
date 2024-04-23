eee9d67467a7b09c40a70c9eb4785d1fd6dad1b4
Dmitry Sidorov <61616100+JustyDev@users.noreply.github.com>
2024-04-23 10:08:01 +0300

    update

--- Semester-2/Теория вероятностей и матстат (16)/2 лаба/Решение/41.py
+++ Semester-2/Теория вероятностей и матстат (16)/2 лаба/Решение/41.py
@@ -1,27 +1,40 @@
-# P = t*(2*T - t) / T**2
-# искомая вероятность в ручном расчёте
+import time
+
+# измерение времени работы
+start_time = time.time()
 
 x, y = 0, 0  # моменты поступления первого и второго сигналов
-T = 20  # длительность работы
-t = 4  # какая-то переменная
+T = 200  # длительность работы
+t = 30  # какая-то переменная
 arr = []  # массив успешных координат
 
-for x in range(T):
-    for y in range(T):
-        if (y < x + t and y > x) or (y > x - t and y < x) or x == y:
+
+# имеется ли нужная координата в массиве координат
+def isExist(sx, sy, s_arr):
+    for xy in s_arr:
+        if sx == xy[0] and sy == xy[1]:
+            return True
+    return False
+
+
+for x in range(0, T):
+    for y in range(0, T):
+        if (y < x + t and y > x) or (y > x - t and y < x):
             arr.append([x, y])
 
 lines = []
 
 for x in range(0, T):
-    line = []
-    for y in range(0, T):
-        f = 0
-        for xy in arr:
-            if x == xy[0] and y == xy[1]:
-                f = 1
-        line.append(f)
+    line = [int(isExist(x, y, arr)) for y in range(0, T)]
     lines.insert(0, line)
 
-for line in lines:
-    print(line)
+print("Схема:")
+[print(l) for l in lines]
+
+execution_time = time.time() - start_time  # вычисляем время выполнения
+
+print("Площадь g:", t * (2 * T - t))
+print("Площадь G:", T ** 2)
+print("Вероятность (g/G):", t * (2 * T - t) / T ** 2)
+print("Вероятность по точкам:", len(arr) / T ** 2)
+print(f"Время выполнения: {execution_time} секунд")
