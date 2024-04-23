a857009d32800d43550a3aa1c0224e809bd0ea68
Dmitry Sidorov <61616100+JustyDev@users.noreply.github.com>
2024-04-23 18:34:39 +0300

    фулл задание 1 в лабе 2

---
+++ Semester-2/Программирование (15)/Labs/src/main/java/ru/justydev/Lab2_2.java
@@ -0,0 +1,28 @@
+package ru.justydev;
+
+import java.util.Scanner;
+
+public class Lab2_2 {
+  public static void main(String[] args) {
+    double x1, x2, x3, x4, x5;
+
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Введите X1: ");
+    x1 = scanner.nextDouble();
+    System.out.print("Введите X2: ");
+    x2 = scanner.nextDouble();
+    System.out.print("Введите X3: ");
+    x3 = scanner.nextDouble();
+    System.out.print("Введите X4: ");
+    x4 = scanner.nextDouble();
+    System.out.print("Введите X5: ");
+    x5 = scanner.nextDouble();
+
+    double a = Math.max(Math.max(x1, x2 + x3), x4 + x5);
+    double b = Math.min(Math.min(Math.max(x1, x2 + 1), x3 + x4), x5);
+
+    double res = (2 * a * a * a - 3 * b) / (Math.sqrt(a * b) + 5 * a);
+
+    System.out.println("Результат = " + res);
+  }
+}
