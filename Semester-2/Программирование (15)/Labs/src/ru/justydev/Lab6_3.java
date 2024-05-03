3b8b49f861b22af29ee7ab3ef06b5c072314103c
Dmitry Sidorov <61616100+JustyDev@users.noreply.github.com>
2024-05-03 18:17:15 +0300

    lab 6

--- Semester-2/Программирование (15)/Labs/src/ru/justydev/Lab6_3.java
+++ Semester-2/Программирование (15)/Labs/src/ru/justydev/Lab6_3.java
@@ -1,32 +1,57 @@
 package ru.justydev;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Lab6_3 {
   public static void main(String[] args) {
 
-    int Xn, Yn, Zn;
+    int N;
     double[] X, Y, Z;
     Scanner inp = new Scanner(System.in);
 
+    //Ввод N
+    System.out.print("N=");
+    N = inp.nextInt();
+
     //Ввод X
-    System.out.print("Xn=");
-    Xn = inp.nextInt();
-    X = new double[Xn];
-    for (int i = 0; i < Xn; i++) {
+    X = new double[N];
+    for (int i = 0; i < N; i++) {
       System.out.print("X(" + i + ") = ");
       X[i] = inp.nextDouble();
     }
 
     //Ввод Y
-    System.out.print("Yn=");
-    Yn = inp.nextInt();
-    Y = new double[Yn];
-    for (int i = 0; i < Yn; i++) {
+    Y = new double[N];
+    for (int i = 0; i < N; i++) {
       System.out.print("Y(" + i + ") = ");
       Y[i] = inp.nextDouble();
     }
 
+    Z = new double[0];
+    for (int k = 0; k < N; k++) {
+      double sum = Y[k] + X[k];
+      if (sum < 0) {
+        Z = Arrays.copyOf(Z, Z.length + 1);
+        Z[Z.length - 1] = sum;
+      }
+    }
 
+    //Сортировка Z по убыванию
+    for (int i = 0; i < Z.length - 2; i++) {
+      int N_min = i;
+      for (int j = i + 1; j < Z.length; j++) {
+        if (Z[i] < Z[N_min]) N_min = j;
+      }
+      double temp = Z[i];
+      Z[i] = Z[N_min];
+      Z[N_min] = temp;
+    }
+
+    //Выводим результат
+    System.out.println("Конечный массив: ");
+    for (int i = 0; i < Z.length; i++) {
+      System.out.printf("Z(%d) = %.2f\n", i, Z[i]);
+    }
   }
 }
