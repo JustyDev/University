4f79d0959de8d6843b111191e41aa4a2f2516323
Dmitry Sidorov <61616100+JustyDev@users.noreply.github.com>
2024-05-07 12:15:22 +0300

    теорвер

--- Semester-2/Теория вероятностей и матстат (16)/3.1 лаба/16-1.py
+++ Semester-2/Теория вероятностей и матстат (16)/3.1 лаба/16-1.py
@@ -20,5 +20,5 @@ execution_time = time.time() - start_time
 
 print("Вероятность (1 < X < 4):", m1 / N)
 print("Вероятность (0 < X < 8):", m2 / N)
-print("Имитация:", m1 / N * m2 / N)
+print("Вероятность:", m2 / N * m1 / N)
 print(f"Время выполнения: {execution_time} секунд")
