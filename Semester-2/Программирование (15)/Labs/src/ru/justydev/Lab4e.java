48bd0f88c6c88e587e3a1c70bfb2924c9316cf9b
Dmitry Sidorov <61616100+JustyDev@users.noreply.github.com>
2024-05-17 10:48:28 +0300

    Update Lab4e.java

--- Semester-2/Программирование (15)/Labs/src/ru/justydev/Lab4e.java
+++ Semester-2/Программирование (15)/Labs/src/ru/justydev/Lab4e.java
@@ -2,24 +2,26 @@ package ru.justydev;
 
 import java.util.Scanner;
 
-public class Lab4_from_example {
+public class Lab4e {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     System.out.print("Введите E: ");
     double E = sc.nextDouble();
+    E *= E;
     System.out.print("Введите X: ");
     double x = sc.nextDouble();
     sc.close();
 
-    double fx = x / 4 * (Math.exp(x) - 2*Math.sin(x) - Math.exp(-x));
+    double sqx = Math.pow(x, 0.5);
+    double fx = (x + 1 - sqx) * Math.exp(sqx) - (x + 1 + sqx) * Math.exp(-sqx);
 
     int k = 1;
-    double R = x * x * x * x / 6;
-    double S = R;
+    double R = x / 6;
+    double S = R * k * k * Math.sqrt(x) * 8;
     do {
       k++;
-      R = R * x * x * x * x / (4 * k - 1) / (4 * k - 2) / (4 * k - 3) / (4 * k - 4);
-      S += R;
+      R = R * x / (2 * k + 1) / (2 * k);
+      S += R * k * k * Math.sqrt(x) * 8;
     } while (Math.abs(R / S) > E && k < 1000);
 
     if (k == 1000) System.out.println("Не посчитано");
