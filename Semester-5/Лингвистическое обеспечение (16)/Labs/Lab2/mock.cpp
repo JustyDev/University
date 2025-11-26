5dd25e95c9cd652ac1b6613c11be0bfd8a1a4434
Dmitry Sidorov <61616100+JustyDev@users.noreply.github.com>
2025-11-27 00:39:58 +0300

    Update mock.cpp

--- Semester-5/Лингвистическое обеспечение (16)/Labs/Lab2/mock.cpp
+++ Semester-5/Лингвистическое обеспечение (16)/Labs/Lab2/mock.cpp
@@ -1,16 +1,25 @@
 // Примеры разных определений MYCONST
 // Допускаются произвольные пробелы и табуляции.
-#define   MYCONST     0xDEAD'BEEFu     // Должно быть: ДА (hex с U и разделителями)
 
-#define MYCONST_BAD1 123               // Десятичная — НЕТ
-#define MYCONST_BAD2 -0x2A             // Унарный минус + hex — НЕТ (это не один литерал)
-#define MYCONST_BAD3 0x1G              // Не валидный hex — НЕТ
-#define MYCONST_BAD4 0xFFuz            // Неверный суффикс — НЕТ
-#define MYCONST_OK2  (0x45LL)          // Скобки вокруг литерала — это ок, но это не MYCONST
-#define MYCONST_OK3  0XABC'123'0lU     // Валидный hex с суффиксами в другом порядке — это ок, но это не MYCONST
+// Должно быть: ДА (hex с U и разделителями)
+#define   MYCONST     0xDEAD'BEEFu
+
+// Десятичная — НЕТ
+#define MYCONST 123
+// Унарный минус + hex — НЕТ (это не один литерал)
+#define MYCONST -0x2A
+// Не валидный hex — НЕТ
+#define MYCONST 0x1G
+// Неверный суффикс — НЕТ
+#define MYCONST 0xFFuz
+// Скобки вокруг литерала — это ок, но это не MYCONST
+#define MYCONST  (0x45LL)
+// Валидный hex с суффиксами в другом порядке — это ок, но это не MYCONST
+#define MYCONST  0XABC'123'0lU
 
 // Переопределение MYCONST (если есть несколько, программа покажет все):
-#define MYCONST ( 0xBADC0FFEEu )       // ДА (скобки снимаются перед проверкой)
+// ДА (скобки снимаются перед проверкой)
+#define MYCONST ( 0xBADC0FFEEu )
 
 int main() {
     return 0;
